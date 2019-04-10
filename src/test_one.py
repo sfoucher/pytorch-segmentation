f0e66aaa882be21284ff1aa13b25eea26dc86ef3
Laurent Chasserat <laurent.chasserat@gmail.com>
2019-04-10 20:54:51 +0000

    Comment test_one code

--- src/test_one.py
+++ src/test_one.py
@@ -24,34 +24,41 @@ batch_size = 1
 model.eval()
 # Deactivate autograd engine to reduce memory usage (no need backprop when inferring)
 with torch.no_grad():
-    print('Loading input image...')
+    print('Loading image...')
     img = cv2.imread(test_image_path, cv2.IMREAD_COLOR)
 
-    print('Displaying input image...')
-
+    """
     cv2.imshow('image', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
+    """
 
     print('Predicting...')
 
+    # Transpose image to fit torch tensor format
     img = img.transpose(2, 0, 1)
+    # Conversion to torch tensor
     img_tensor = torch.tensor([img])
+    # Conversion to float to fit torch model
     img_tensor = img_tensor.float()
-    print('A'+str(type(img_tensor)))
-    print(img_tensor.size())
+    # Print tensor size
+    print('Input tensor size : ' + str(img_tensor.size()))
 
     # Send to CPU or GPU depending on the hardware found
     img_tensor = img_tensor.to(device)
 
     # Generate predictions
     preds = model.tta(img_tensor, net_type='deeplab')
-    preds = preds.argmax(dim=1)
-
+    print(preds.size())
+    # Not sure what this does in initial code, probably helps extracting a single class
+    # preds = preds.argmax(dim=1)
     print(preds.size())
 
+    # Convert back to nparray procssable as an image
     preds_np = preds.detach().cpu().numpy()
 
     print(type(preds_np))
+
+    # Probably need to transform preds to a png now !
+
 
